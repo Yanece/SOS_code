e5279dae0c18e9959ef0f013e326734d12443d96
Semakia <98928019+Semakia@users.noreply.github.com>
2024-05-14 10:04:28 +0200

    Update index.js

--- sos-code/src/index.js
+++ sos-code/src/index.js
@@ -6,7 +6,7 @@ import Zone from "./Text_zone";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+          <App />
   </React.StrictMode>
 );
 
