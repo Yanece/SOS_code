df26a51959356a4d53f86d84e34ba700f7536037
Jordan Ngueko <j53276019@gmail.com>
2024-05-24 20:09:28 +0200

    Menu deroulant

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,30 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class Menu extends Component {
-  render() {
-    return (
-      <h1>Select language</h1>
-    );
-  }
-}
+const Menu = () => {
+  const [Open, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!Open);
+  };
+
+  return (
+    <div>
+      <button onClick={toggleMenu}>
+        Sélectionnez une langue {Open? '▲' : '▼'}
+      </button>
+      {Open && (
+        <ul>
+          <li>Python</li>
+          <li>JavaScrit</li>
+          <li>C</li>
+          <li>JavaScrit</li>
+          <li>JavaScrit</li>
+          <li>JavaScrit</li>
+          <li>JavaScrit</li>
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default Menu;
--- src/Title.js
+++ src/Title.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 
-
 class Title extends Component {
   render() {
     return (
-      <div>
-        <h1> Welcome to S.o.S Code </h1>
+      <div class="title-container">
+        <h1 class="title">Welcome to SOS~CODE</h1>
       </div>
     );
   }
